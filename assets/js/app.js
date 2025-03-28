20a17c84b19f1a56b2559a6cfeb3fc95f40d1f3f
jatin-rajput563 <jkt3298@gmail.com>
2025-03-28 15:47:15 +0530

    familiar-section-first-slider-complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,37 @@
 const navbarbtn = () => {
     document.getElementById("nav-name").classList.toggle("show-navbar")
     document.body.classList.toggle("overflow-hidden")
-}
+};
+$('.left-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 6.2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
