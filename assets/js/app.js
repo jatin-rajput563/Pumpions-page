a643c08029e6e89a26e928468f9fda492cfc8a2c
jatin-rajput563 <jkt3298@gmail.com>
2025-04-04 15:58:31 +0530

    changes-in-all-sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,55 +2,25 @@ const navbarbtn = () => {
     document.getElementById("nav-name").classList.toggle("show-navbar")
     document.body.classList.toggle("overflow-hidden")
 };
-$('.left-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 800,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 100,
-    // responsive: [
-    //     {
-    //         breakpoint: 480,
-    //         settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1
-    //         }
-    //     }
-    // ]
-});
-$('.right-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 800,
-    slidesToShow: 6.2,
-    slidesToScroll: 1,
-    autoplay: true,
-    rtl: true,
-    autoplaySpeed: 100,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
 
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
+
+// slider 1
+$('.slider').slick({
+    slidesToShow: 1.2,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 800,
+    dots: true,
+});
+
+
+// slider 2
+$('.team-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 800,
+    dots: true,
 });
